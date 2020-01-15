ebc5963bf9f402d2e1d6794040b247bb1c6c3621
tzvikif <tzvikif@gmail.com>
2020-01-15 23:19:27 +0200

    syntax

--- script.m
+++ script.m
@@ -1,9 +1,12 @@
 %%authors: דפנה קופל 209051036 , צבי פישר 037580644
 %%
 close all;
+clear;
 im = readImage('wolf.tif');
 Fim = fft2(im);
 showFFT(Fim);
+fprintf('as we can see there are 4 points of interest.\n');
+mypause;
 % as we can see there are 4 points of interest.
 magnitudeFim = abs(Fim);
 magnitudeFim(1,1) = 0;
@@ -13,7 +16,7 @@ M = max(magnitudeFim,[],'all');
 % now i know the indices of that frequency(row,col). in order not to cancel that
 % frequency(insert zero) i use the mean.
 r = 2;
-averaged = cleanImageMean(Fim(u(1)-r:u(1)+r,v(1)-r:v(1)+r),radius,2);
+averaged = cleanImageMean(Fim(u(1)-r:u(1)+r,v(1)-r:v(1)+r),r,2);
 averaged = averaged(r+1,r+1);
 Fim(u(1),v(1)) = averaged;
 averaged2 = conj(averaged);
@@ -21,7 +24,7 @@ Fim(u(2),v(2)) = averaged2;
 % i found that those are the frequency is at:
 u1 = 20;v1 = 226;
 u2 = 238;v2 = 32;
-averaged = cleanImageMean(Fim(u1(1)-r:u1(1)+r,v1(1)-r:v1(1)+r),radius,2);
+averaged = cleanImageMean(Fim(u1(1)-r:u1(1)+r,v1(1)-r:v1(1)+r),r,2);
 averaged = averaged(r+1,r+1);
 Fim(u1,v1) = averaged;
 Fim(u2,v2) = conj(averaged);
